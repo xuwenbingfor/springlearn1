2ead7d5acee1231bc0540150d5d28657f4348c7d
xuwenbingfor <1965104943@qq.com>
2020-04-02 21:51:20 +0800

    测试SpringFactoriesLoader

--- src/test/java/com/jz/spring/ApplicationTests.java
+++ src/test/java/com/jz/spring/ApplicationTests.java
@@ -1,13 +1,28 @@
 package com.jz.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.support.SpringFactoriesLoader;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Enumeration;
 
 @SpringBootTest
+@Slf4j
 class ApplicationTests {
 
     @Test
-    void contextLoads() {
+    void test1() throws IOException {
+        // 1、获取类路径下对应资源文件，会扫描所有jar包
+        Enumeration<URL> resources = this.getClass()
+                .getClassLoader()
+                .getResources(SpringFactoriesLoader.FACTORIES_RESOURCE_LOCATION);
+        while (resources.hasMoreElements()) {
+            URL resource = resources.nextElement();
+            log.info(resource.toString());
+        }
     }
 
 }
