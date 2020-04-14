1ca2f86557cb24a9a0d87ec07026d3554a5f4760
xuwenbingfor <1965104943@qq.com>
2020-04-14 22:22:53 +0800

    Lock

---
+++ src/test/java/com/jz/java/concurrent/LockTests.java
@@ -0,0 +1,54 @@
+package com.jz.java.concurrent;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+@Slf4j
+public class LockTests {
+    @Test
+    public void test1() throws InterruptedException {
+        Counter counter = new Counter();
+        for (int i = 0; i < 10000; i++) {
+            new Thread(() -> {
+                counter.addOne();
+            }).start();
+        }
+        TimeUnit.SECONDS.sleep(50);
+        counter.printCount();
+    }
+}
+
+/**
+ * 计数器
+ */
+@Slf4j
+class Counter {
+    private Integer count = 0;
+    private Lock lock = new ReentrantLock();
+
+    // 存在并发问题
+//    public void addOne() {
+//        log.info("线程{} addOne 开始", Thread.currentThread().getId());
+//        this.count++;
+//        log.info("线程{} addOne 结束", Thread.currentThread().getId());
+//    }
+
+    public void addOne() {
+        lock.lock();
+        try {
+            log.info("线程{} addOne 开始", Thread.currentThread().getId());
+            this.count++;
+            log.info("线程{} addOne 结束", Thread.currentThread().getId());
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void printCount() {
+        log.info("count:{}", this.count);
+    }
+}
