52c7c570d869fe2c7f6b37d1f9e91debbed8affc
xuwenbingfor <1965104943@qq.com>
2020-04-18 22:38:24 +0800

    ReentrantReadWriteLock测试

--- src/test/java/com/jz/java/concurrent/ReadWriteLockTests.java
+++ src/test/java/com/jz/java/concurrent/ReadWriteLockTests.java
@@ -14,6 +14,24 @@ import java.util.concurrent.locks.ReentrantReadWriteLock;
 @Slf4j
 public class ReadWriteLockTests {
     @Test
+    public void test2() throws InterruptedException {
+        // 读-读能共存
+        // 读-写不能共存
+        // 写-写不能共存
+        Cache cache = new Cache();
+        new Thread(() -> {
+            cache.get("key");
+        }).start();
+        TimeUnit.SECONDS.sleep(1);
+        log.info("1s后启动写线程");
+        new Thread(() -> {
+            cache.put("key", "value");
+        }).start();
+        TimeUnit.MINUTES.sleep(5);
+        log.info("主线程结束");
+    }
+
+    @Test
     public void test1() throws InterruptedException {
         Cache cache = new Cache();
         for (int i = 0; i < 5; i++) {
@@ -45,7 +63,7 @@ public class ReadWriteLockTests {
             readLock.lock();
             try {
                 log.info("线程:{}读", Thread.currentThread().getId());
-                TimeUnit.SECONDS.sleep(5);
+                TimeUnit.SECONDS.sleep(10);
                 return cache.get(key);
             } catch (Exception e) {
                 log.error("error", e);
