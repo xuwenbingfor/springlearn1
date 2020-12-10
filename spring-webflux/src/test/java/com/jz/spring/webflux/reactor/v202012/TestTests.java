b37458ef9cd6a0a8b56cec5aab70d019dcf64115
xuwenbingfor <1965104943@qq.com>
2020-12-10 22:49:24 +0800

    flux test

---
+++ spring-webflux/src/test/java/com/jz/spring/webflux/reactor/v202012/TestTests.java
@@ -0,0 +1,17 @@
+package com.jz.spring.webflux.reactor.v202012;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author xuwenbingfor
+ * @version 2020/12/10 22:31
+ * @description
+ */
+@Slf4j
+public class TestTests {
+    @Test
+    public void test1() {
+
+    }
+}
